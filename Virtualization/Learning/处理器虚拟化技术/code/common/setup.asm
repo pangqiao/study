6925d98bac7f6e5a1e6b879c17e2fc068fed3df1
Haiwei Li <lihaiwei.kernel@gmail.com>
2020-02-11 23:04:49 +0800

    --- Auto-Git Commit ---

---
+++ Virtualization/Learning/处理器虚拟化技术/code/common/setup.asm
@@ -0,0 +1,372 @@
+;*************************************************
+; setup.asm                                      *
+; Copyright (c) 2009-2013 邓志                   *
+; All rights reserved.                           *
+;*************************************************
+
+
+;;
+;; 这个 setup 模式是共用的，放在 ..\common\ 目录下
+;; 用于写入磁盘的第 1 号扇区 ！
+;;
+
+%include "..\inc\support.inc"
+%include "..\inc\protected.inc"
+%include "..\inc\system_manage_region.inc"
+%include "..\inc\apic.inc"
+
+
+;;
+;; 说明：
+;; 1) 模块开始点是 SETUP_SEGMENT
+;; 2) 模块头的存放是“模块 size”
+;; 3) load_module() 函数将模块加载到 SETUP_SEGMENT 位置上
+;; 4) SETUP 模块的“入口点”是：SETUP_SEGMENT + 0x18
+        
+        [SECTION .text]
+        org SETUP_SEGMENT
+
+
+;;
+;; 在 0x8000 区域定义引导管理块 (LOADER_BLOCK)
+;;         
+SetupLenth      DD SETUP_LENGTH                 ; 这个模块的 size
+CpuIndex        DD 0FFFFFFFFh                   ; CPU index
+CurrentVideo    DD 0B8000h					    ; CurrentVideo
+ApStage1Lock    DD 1                            ; stage1（setup）阶段的锁
+ApStage2Lock    DD 1                            ; stage2（protected）阶段的锁
+ApStage3Lock    DD 1                            ; stage3（long）阶段的锁    
+MMap.Size       DQ 0
+MMap.Base:      DQ 0                            ; 内存区域的地起地址
+MMap.Length:    DQ 0                            ; 内存区域的长度
+MMap.Type:      DD 0                            ; 内存区域的类型:
+
+
+;;
+;; 模块当前运行在 16 位实模式下
+;;
+        bits 16
+        
+SetupEntry:                                             ; 这是模块代码的入口点。
+        cli
+        cld
+        NMI_DISABLE
+        call enable_a20  
+        DISABLE_8259 
+
+        lock inc DWORD [CpuIndex]
+        call check_cpu_environment
+        call get_system_memory
+        call protected_mode_enter
+                
+
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ;;;;;;;;;;; 下面是 32 位代码 ;;;;;;;;;;;;;;;;;;;
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        
+        bits 32
+
+        ;;
+        ;; 下面进行数据的初始化设置:
+        ;; 1) 首先，初始化 SDA（System Data Area）区域数据
+        ;; 2) 然后，初始化 PCB（Processor Control Block）区域数据
+        ;;
+        ;; 说明:
+        ;; 1) SDA 数据是所有处理器共享，所以必须先初始化
+        ;; 2) PCB 数据是 logical processor 数据，共支持 16 个 PCB 块
+        ;; 3) PCB 数据是动态分配，每个 PCB 块基址不同
+        ;; 
+        ;;
+        ;; fs 段说明：
+        ;;      1) fs 指向 SDA（System Data Area）区域，是所有 logical processor 共享的数据区域
+        ;; 注意：
+        ;;      1) 需要在支持 64 位的处理器上才能直接写 IA_FS_BASE 寄存器！
+        ;;      2) 否则，需要开启保护模式来加载 FS 段基址
+        ;;      3) GS 段基址在后续代码中更新
+        ;;        
+        call init_system_data_area
+
+
+PcbInitEntry:
+        ;;
+        ;; 设置 PCB（Processor Control Block）内容
+        ;; 说明：
+        ;; 1) 此处为 logical processor 的 PCB 初始化入口（包括 BSP 与 AP）
+        ;; 2) 每个 logical processor 都需要经过下面的 PCB 数据初始化
+        ;; 
+        call init_processor_control_block
+        call init_apic
+        call init_processor_basic_info
+        call init_processor_topology_info
+        call init_debug_capabilities_info
+        call init_memory_type_manage
+        call init_perfmon_unit
+
+%ifndef DBG
+        ;;
+        ;; Stage1 阶段最后工作，检查是否为 BSP
+        ;; 1) 是，则发送 INIT-SIPI-SIPI 序列
+        ;; 2) 否，则等待接收 SIPI 
+        ;;
+        cmp BYTE [gs: PCB.IsBsp], 1
+        jne ApStage1End
+
+        ;;
+        ;; 这是 BSP 第1阶段的最后工作：
+        ;; 1) 发送 INIT-SIPI-SIPI 序列给 AP 
+        ;; 2) 等待所有 AP 第1阶段完成
+        ;; 3) 转入下阶段工作
+        ;;
+        call wait_for_ap_stage1_done
+%endif         
+
+        ;;
+        ;; 检查是否需要进入 longmode
+        ;; 1) 是，跳过 stage2, 进入 stage3 阶段（longmode 模式）
+        ;; 2) 否，进入 stage2 阶段
+        ;;
+        cmp DWORD [fs: SDA.ApLongmode], 1
+        mov eax, [PROTECTED_SEGMENT+4]
+        cmove eax, [LONG_SEGMENT+4]
+        jmp eax
+
+
+%ifndef DBG      
+        ;;
+        ;; AP第1阶段最后工作说明：
+        ;; 1) 增加 ApInitDoneCount 计数值
+        ;; 1) AP 等待第2阶段锁（等待 BSP 开放 stage2 锁）
+        ;;
+        
+ApStage1End:  
+        ;;
+        ;; 增加完成计数
+        ;;
+        lock inc DWORD [fs: SDA.ApInitDoneCount]
+
+        ;;
+        ;; 开放第1阶段 AP Lock
+        ;;
+        xor eax, eax
+        mov ebx, [fs: SDA.Stage1LockPointer]
+        xchg [ebx], eax
+
+%ifdef TRACE
+	call dump_trace_message
+%endif
+        ;;
+        ;; 检查是否需要进入 longmode
+        ;; 1) 是，跳过 stage2, 等待 stage3 锁，进入 stage3 阶段（longmode 模式）
+        ;; 2) 否，等待 stage2 锁，进入 stage2 阶段
+        ;;
+        cmp DWORD [fs: SDA.ApLongmode], 1
+        je ApStage1End.WaitStage3
+
+        ;;
+        ;; 现在等待 stage2 的锁开放
+        ;;
+        mov esi, [fs: SDA.Stage2LockPointer]
+        call get_spin_lock
+        ;;
+        ;; 进入 stage2
+        ;;
+        jmp [PROTECTED_SEGMENT+8]
+        
+ApStage1End.WaitStage3:
+        ;;
+        ;; 现在等待 stage3 锁开放
+        ;;
+        mov esi, [fs: SDA.Stage3LockPointer]
+        call get_spin_lock                
+        ;;
+        ;; 进入 stage3
+        ;; 
+        jmp [LONG_SEGMENT+8]
+
+
+%endif
+
+    
+
+;$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+;$      AP Stage1 Startup Routine       $
+;$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+        bits 16
+
+times 4096 - ($ - $$)   DB      0
+
+
+ApStage1Entry:
+
+        cli
+        cld
+
+        ;;
+        ;; real mode 初始环境
+        ;;
+        xor ax, ax
+        mov ds, ax
+        mov es, ax
+        mov ss, ax
+        mov sp, BOOT_SEGMENT
+        jmp WORD 0 : ApStage1Entry.Next
+ApStage1Entry.Next:        
+       
+        ;;
+        ;; 检查 ApLock，是否允许 AP 进入 startup routine 执行
+        ;;
+        xor eax, eax
+        xor esi, esi
+        inc esi
+        
+        ;;
+        ;; 获得自旋锁
+        ;;
+AcquireApStage1Lock:
+        lock cmpxchg [ApStage1Lock], esi
+        jz AcquireApStage1LockOk
+        
+CheckApStage1Lock:
+        mov eax, [ApStage1Lock]
+        test eax, eax 
+        jz AcquireApStage1Lock
+        pause
+        jmp CheckApStage1Lock
+        
+
+        
+AcquireApStage1LockOk:
+        ;;
+        ;; AP stage1 前期处理
+        ;;
+        lock inc DWORD [CpuIndex]
+        call check_cpu_environment
+        call protected_mode_enter
+
+
+
+        bits 32
+        
+%if 0
+;; 下面是为测试而增加 !!
+        call init_processor_control_block
+        call init_apic
+        call init_processor_basic_info
+        call init_processor_topology_info
+        call init_debug_capabilities_info
+        call init_memory_type_manage
+        call init_perfmon_unit
+
+
+        ;;
+        ;; 增加完成计数
+        ;;
+        lock inc DWORD [fs: SDA.ApInitDoneCount]
+        ;;
+        ;; 开放第1阶段 AP Lock
+        ;;
+        xor eax, eax
+        mov ebx, [fs: SDA.Stage1LockPointer]
+        xchg [ebx], eax
+
+call dump_trace_message
+        ;;
+        ;; 现在等待 stage2 的锁开放
+        ;;
+        mov esi, [fs: SDA.Stage2LockPointer]
+        call get_spin_lock
+
+        ;;
+        ;; 进入 stage2
+        ;;
+        jmp [PROTECTED_SEGMENT+8]
+        
+
+%endif
+        ;;
+        ;; 转入 PCB 初始化
+        ;; 
+        mov eax, PcbInitEntry
+        jmp eax
+        
+
+   
+
+;-----------------------------------------
+; dump_trace_message():
+; 说明:
+;       打印 trace 信息
+;-----------------------------------------
+dump_trace_message:
+        mov esi, Status.CpuIdMsg        
+        call __puts
+        mov esi, [gs: PCB.LogicalId]
+        call __dump_hex
+        mov esi, ','
+        call __putc
+        cmp DWORD [fs: SDA.ApLongmode], 1
+        mov esi, Stage1.Msg2
+        je dump_trace_message.@1
+        mov esi, Stage1.Msg1
+dump_trace_message.@1:
+        call __puts
+        ret
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 下面是 include 进来的函数模块        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        bits 16
+                
+%include "..\lib\crt16.asm"                 
+ 
+  
+        bits 32
+;;
+;; 下面代码使用在 stage1 阶段
+;;        
+%include "..\lib\print.asm"
+%include "..\lib\crt.asm"        
+%include "..\lib\LocalVideo.asm"
+%include "..\lib\system_data_manage.asm" 
+%include "..\lib\mem.asm"
+%include "..\lib\page32.asm"
+%include "..\lib\apic.asm"
+%include "..\lib\ioapic.asm"
+%include "..\lib\pci.asm"
+%include "..\lib\mtrr.asm"
+%include "..\lib\debug.asm"
+%include "..\lib\perfmon.asm"
+%include "..\lib\pic8259a.asm"
+%include "..\lib\smp.asm"
+%include "..\lib\stage1.asm"
+%include "..\lib\services.asm"
+%include "..\lib\data.asm"
+
+
+
+
+
+        [SECTION .data]
+        ALIGN 16
+
+;;
+;; stage1 的 GDT 与 TSS 区域
+;;
+setup.Gdt     times (16*128)  DB 0
+setup.Tss     times (16*128)  DB 0
+
+        
+;;
+;; 模块长度
+;;
+SETUP_LENGTH    EQU     $ - SETUP_SEGMENT
+
+
+
+; end of setup        
---
+++ Virtualization/Learning/处理器虚拟化技术/code/lib/page32.asm
@@ -0,0 +1,403 @@
+;*************************************************
+; page32.asm                                     *
+; Copyright (c) 2009-2013 邓志                   *
+; All rights reserved.                           *
+;*************************************************
+
+
+%include "..\inc\page.inc"
+
+
+
+
+
+        bits 32
+
+
+
+
+;-------------------------------------------------------------
+; clear_8m_for_pae(): 清 8M 区域（PAE 模式使用的页转换表）
+; input:
+;       esi - address
+;-------------------------------------------------------------
+clear_8m_for_pae:
+        push ecx
+        mov ecx, (8 * 1024 * 1024) / 4096
+clear_8m_for_pae.loop:        
+        call clear_4k_buffer
+        add esi, 4096
+        dec ecx
+        jnz clear_8m_for_pae.loop
+        pop ecx
+        ret
+        
+        
+;--------------------------------------------------
+; get_pte_offset()
+; input:
+;       esi - virutal address or physical address
+; output:
+;       eax - pte address offset
+;-------------------------------------------------- 
+get_pte_offset:
+        mov eax, esi
+        shr eax, 12                             ; get pte index
+        shl eax, 3                              ; pte index * 8
+        ret
+        
+;--------------------------------------------------
+; get_pte_virtual_address()
+; input:
+;       esi - virutal address
+; output:
+;       eax - virtual address of pte address
+;--------------------------------------------------        
+get_pte_virtual_address:
+        mov eax, esi
+        shr eax, 12
+        shl eax, 3
+        add eax, [fs: SDA.PtBase]
+        ret
+
+;--------------------------------------------------
+; get_pte_physical_address()
+; input:
+;       esi - physical address
+; output:
+;       eax - physical address of pte address
+;--------------------------------------------------        
+get_pte_physical_address:
+        mov eax, esi
+        shr eax, 12
+        shl eax, 3
+        add eax, [fs: SDA.PtPhysicalBase]
+        ret
+
+        
+
+
+;-------------------------------------------------------------------
+; load_pdpt()
+; input:
+;       esi - physical address of PDPT(page directory pointer table)
+; output:
+;       eax - pde address
+;------------------------------------------------------------------
+load_pdpt:
+        push ecx
+        push ebx
+        mov ebx, esi
+        xor ecx, ecx
+
+        ;;
+        ;; 将 PDPT 地址调整到 32 bytes 对齐
+        ;;
+        add ebx, 31
+        and ebx, 0FFFFFFE0h
+
+        ;;
+        ;; 保存在 PDPT 表里
+        ;;
+        mov eax, [fs: SDA.PdtPhysicalBase]                      ; PDT 表物理地址
+        and eax, 0FFFFF000h
+        or eax, P                                               ; PDPTE0
+        mov [ebx], eax
+        add eax, 1000h                                          ; PDPTE1
+        mov [ebx + 4], ecx
+        mov [ebx + 8], eax
+        add eax, 1000h                                          ; PDPTE2
+        mov [ebx + 8 + 4], ecx
+        mov [ebx + 16], eax
+        add eax, 1000h                                          ; PDPTE3       
+        mov [ebx + 16 + 4], ecx
+        mov [ebx + 24], eax
+        mov [ebx + 24 + 4], ecx
+        mov cr3, ebx
+        pop ebx
+        pop ecx
+        ret
+
+
+
+;-----------------------------------------------------------------
+; init_pae_ppt:
+; input:
+;       none
+; output:
+;       none
+; 描述:
+;       1) 初始化处理器的 PPT 表
+;-----------------------------------------------------------------
+init_pae_ppt:
+        mov DWORD [gs: PCB.Ppt], PDT0_PHYSICAL_BASE | P
+        mov DWORD [gs: PCB.Ppt+4], 0
+        mov DWORD [gs: PCB.Ppt+8], PDT1_PHYSICAL_BASE | P
+        mov DWORD [gs: PCB.Ppt+12], 0
+        mov DWORD [gs: PCB.Ppt+16], PDT2_PHYSICAL_BASE | P
+        mov DWORD [gs: PCB.Ppt+20], 0
+        mov DWORD [gs: PCB.Ppt+24], PDT3_PHYSICAL_BASE | P
+        mov DWORD [gs: PCB.Ppt+28], 0
+        ret
+
+
+;-----------------------------------------------------------------
+; init_pae_page():
+; input:
+;       none
+; output:
+;       none
+; 描述:
+;       1) 映射页转换表结构区域
+;       2) 使用 4K 页面
+;-----------------------------------------------------------------
+init_pae_page:
+        push ecx
+
+        ;;
+        ;; 清页转换表区域(8M)
+        ;;
+        mov esi, [fs: SDA.PtPhysicalBase]
+        call clear_8m_for_pae
+
+        ;;
+        ;; 映射PAE页转换表结构
+        ;;
+        mov esi, PT_PHYSICAL_BASE | P
+        mov edi, PDT_PHYSICAL_BASE
+        xor ecx, ecx
+        xor eax, eax
+map_pae_page_transition_table.loop:
+        mov [edi+ecx*8], esi
+        mov [edi+ecx*8+4], eax
+        add esi, 1000h
+        inc ecx
+        cmp ecx, 4000h / 8
+        jb map_pae_page_transition_table.loop 
+
+        pop ecx
+        ret
+        
+        
+
+
+        
+;-----------------------------------------------------------
+; do_virtual_address_mapping32(): 执行虚拟地址映射
+; input:
+;       esi - virtual address
+;       edi - physical address
+;       eax - attribute
+; output:
+;       0 - succssful, 否则返回错误码
+;
+; desciption:
+;       eax 传递过来的 attribute 由下面标志位组成：
+;       [0] - P
+;       [1] - R/W
+;       [2] - U/S
+;       [3] - PWT
+;       [4] - PCD
+;       [5] - A
+;       [6] - D
+;       [7] - PAT
+;       [8] - G
+;       [12] - ingore
+;       [28] - ingore
+;       [29] - FORCE，置位时，强制进行映射
+;       [30] - PHYSICAL，置位时，表示基于物理地址进行映射（用于初始化时）
+;       [31] - XD
+;----------------------------------------------------------
+do_virtual_address_mapping32:
+        push ecx
+        push ebx
+        push edx
+        push esi    
+        push edi
+        
+        and esi, 0FFFFF000h
+        and edi, 0FFFFF000h
+        mov ecx, eax
+        
+                
+        ;;
+        ;; PT_BASE - PT_TOP 区域已经初始化映射，不能再进行映射
+        ;; 假如映射到 PT_BASE - PT_TOP 区域内就失败返回
+        ;;
+        cmp esi, [fs: SDA.PtBase]
+        jb do_virtual_address_mapping32.next
+        cmp esi, [fs: SDA.PtTop]
+        ja do_virtual_address_mapping32.next
+
+        mov eax, MAPPING_USED
+        jmp do_virtual_address_mapping32.done
+        
+
+do_virtual_address_mapping32.next:
+        ;;
+        ;; 读取 PTE 地址
+        ;;
+        test ecx, PHY_ADDR                              ; physical address 标志位
+        mov eax, get_pte_virtual_address
+        mov edx, get_pte_physical_address
+        cmovnz eax, edx
+        call eax
+        mov ebx, eax
+
+        ;;
+        ;; 检查 present 标志位
+        ;;
+        test DWORD [ebx], P
+        jz do_virtual_address_mapping32.set_pte
+        test ecx, FORCE
+        jnz do_virtual_address_mapping32.set_pte
+        
+        ;;
+        ;; 已经在使用, 返回出错状态
+        ;;
+        mov eax, MAPPING_USED
+        jmp do_virtual_address_mapping32.done
+
+
+        ;;
+        ;; 设置 PTE
+        ;; 
+do_virtual_address_mapping32.set_pte:
+        mov edx, [fs: SDA.XdValue]
+        and edx, ecx
+        mov eax, ecx
+        and eax, 1FFh
+        or eax, edi
+
+        ;; 写入 PTE 值
+        mov [ebx], eax
+        mov [ebx+4], edx
+
+        mov eax, MAPPING_SUCCESS
+
+do_virtual_address_mapping32.done:
+        pop edi
+        pop esi
+        pop edx
+        pop ebx
+        pop ecx
+        ret
+
+
+;-----------------------------------------------------------
+; do_virtual_address_mapping32_n()
+; input:
+;       esi - virtual address
+;       edi - physical address
+;       eax - attribute
+;       ecx - n 个页面
+; output:
+;       0 - succssful, 否则返回错误码
+; 描述：
+;       1) 映射 n 个 页面
+;-----------------------------------------------------------
+do_virtual_address_mapping32_n:
+        push ecx
+        push edx
+        mov edx, eax
+
+do_virtual_address_mapping32_n.loop:        
+        mov eax, edx
+        call do_virtual_address_mapping32
+        add esi, 1000h
+        add edi, 1000h
+        dec ecx
+        jnz do_virtual_address_mapping32_n.loop
+        
+        pop edx
+        pop ecx
+        ret
+
+
+
+        
+
+;-----------------------------------------------------------
+; query_physical_page()
+; input:
+;       esi - va
+; output:
+;       edx:eax - 物理页面，失败时，返回 -1
+; 描述：
+;       1) 查询虚拟地址映射的物理页面
+;       2) 当虚拟地址无映射页面时，返回 -1 值
+;-----------------------------------------------------------
+query_physical_page:
+        push ebx
+        
+        
+        ;;
+        ;; 读 PTE 值
+        ;;
+        call get_pte_virtual_address
+        mov edx, [eax + 4]
+        mov eax, [eax]
+        mov esi, -1
+        and edx, [gs: PCB.MaxPhyAddrSelectMask + 4]
+        
+        ;;
+        ;; 检查 PTE 是否有效
+        ;;
+        test eax, P
+        cmovz edx, esi
+        cmovz eax, esi
+        jz query_physical_page.done
+        
+query_physical_page.ok:
+        and eax, 0FFFFF000h
+        
+query_physical_page.done:        
+        pop ebx
+        ret        
+        
+        
+
+
+;-----------------------------------------------------------
+; do_virtual_address_unmapped(): 解除虚拟地址映射
+; input:
+;       esi - virtual address
+; output:
+;       0 - successful,  otherwise - error code
+;-----------------------------------------------------------
+do_virtual_address_unmapped:
+        push ecx
+
+        ;;
+        ;; PT_BASE - PT_TOP 区域不能被解除映射
+        ;; 假如映射到 PT_BASE - PT_TOP 区域内就失败返回
+        ;;
+        cmp esi, [fs: SDA.PtBase]
+        jb do_virtual_address_unmapped.next
+        cmp esi, [fs: SDA.PtTop]
+        ja do_virtual_address_unmapped.next 
+        mov eax, UNMAPPING_ADDRESS_INVALID
+        jmp do_virtual_address_unmapped.done
+        
+do_virtual_address_unmapped.next:    
+        call get_pte_virtual_address
+        mov ecx, [eax]
+        test ecx, P
+        jz do_virtual_address_unmapped.ok
+
+        xor ecx, ecx
+        xchg [eax], ecx
+        invlpg [esi]                                    ; 刷新 TLB
+
+do_virtual_address_unmapped.ok:
+        mov eax, UNMAPPED_SUCCESS        
+
+do_virtual_address_unmapped.done:
+        pop ecx
+        ret
+                
+
+
+
+        
+
