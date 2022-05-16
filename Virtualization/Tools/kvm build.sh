69df5473e4a88da924df0e6ec9eb40ef5ae31966
Haiwei Li <haiwei-li@outlook.com>
2022-05-16 23:18:53 +0800

    --- Auto-Git Commit ---

--- Virtualization/Tools/kvm build.sh
+++ Virtualization/Tools/kvm build.sh
@@ -3,7 +3,7 @@ set -o nounset                              # Treat unset variables as an error
 
 sudo rmmod kvm-intel
 sudo rmmod kvm
-rm `pwd`/arch/x86/kvm/*.o
+sudo rm `pwd`/arch/x86/kvm/*.o
 sudo make -j8 CONFIG_KVM=m CONFIG_KVM_INTEL=m -C `pwd` M=`pwd`/arch/x86/kvm modules
 #sudo make -j8 CONFIG_KVM=m CONFIG_KVM_INTEL=m -C /lib/modules/`uname -r`/build M=`pwd`/arch/x86/kvm/ modules
 sudo insmod `pwd`/arch/x86/kvm/kvm.ko 
